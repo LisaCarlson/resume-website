8c1e7d895e6ef05ad50a5a7667d4735127a16d15
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-14 22:35:48 -0700

    About Section complete

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,20 +1,19 @@
 $( document ).ready(function() {
-   $(window).scroll(function(){
-    if ($(".navbar").offset().top>50) {
+  $('.flower1,.flower2').hide();
+   $(window).scroll(function() {
+    console.log($(".navbar").offset().top)
+    if ($(".navbar").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     }
     else {
       $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-
+    if ($(".navbar").offset().top > 765) {
+      $('.flower1, .flower2').fadeIn(2000);
+      setTimeout(function(){
+        $('.flower1, .flower2').css({'transform':'scale(1.5)', 'transition': 'cubic-bezier(.17,.67,.83,.67)', 'transition-duration':'2s'});
+      }, 1000); 
+    }
   });
-
-   $('button.collapsed').on('shown.bs.collapse', function () {
-     console.log('shown')
-   })
-   // $('figure:hover').addClass('animated bounceOutLeft');
-      // $('figure').hover(function() {
-      //   $(this).find('figcaption').toggleClass('slide');
-      // })
 
 });
