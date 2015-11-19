e0bc35771426483c54b98b30b1d7555064737bea
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-18 21:24:19 -0700

    styling changes

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,14 +1,13 @@
 $( document ).ready(function() {
   $('.flower1,.flower2').hide();
    $(window).scroll(function() {
-    console.log($(".navbar").offset().top)
     if ($(".navbar").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     }
     else {
       $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-    if ($(".navbar").offset().top > 765) {
+    if ($(".navbar").offset().top > 715) {
       $('.flower1, .flower2').fadeIn(2000);
       setTimeout(function(){
         $('.flower1, .flower2').css({'transform':'scale(1.5)', 'transition': 'cubic-bezier(.17,.67,.83,.67)', 'transition-duration':'2s'});
